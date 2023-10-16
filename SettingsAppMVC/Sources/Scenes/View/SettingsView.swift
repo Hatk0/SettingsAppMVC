d4442ed3c9f489cfd41991bcf8804d0d58ec7798
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-16 19:09:56 +0300

    Set up SettingsView

--- SettingsAppMVC/Sources/Scenes/View/SettingsView.swift
+++ SettingsAppMVC/Sources/Scenes/View/SettingsView.swift
@@ -1,1 +1,36 @@
-import Foundation
+import UIKit
+import SnapKit
+
+final class SettingsView: UIView {
+    
+    // MARK: - UI
+
+    
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        backgroundColor = .systemBackground
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    private func setupHierarchy() {
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
