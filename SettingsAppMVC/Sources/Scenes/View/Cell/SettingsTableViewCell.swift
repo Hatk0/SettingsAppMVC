feca184782d0ea0696decaad60e32bd11c2b8883
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-16 19:20:27 +0300

    Create settingImageView, make constraints and make the background color appear

--- SettingsAppMVC/Sources/Scenes/View/Cell/SettingsTableViewCell.swift
+++ SettingsAppMVC/Sources/Scenes/View/Cell/SettingsTableViewCell.swift
@@ -5,7 +5,23 @@ class SettingsTableViewCell: UITableViewCell {
 
     static let identifier = "SettingsTableViewCell"
     
+    var model: Settings? {
+        didSet {
+            settingImageView.image = UIImage(systemName: model?.image ?? "")
+            setupBackgroundColor()
+        }
+    }
+    
     // MARK: - UI
+    
+    private lazy var settingImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.layer.cornerRadius = 10
+        imageView.contentMode = .center
+        imageView.clipsToBounds = true
+        return imageView
+    }()
     
     // MARK: - Initializers
     
@@ -22,16 +38,46 @@ class SettingsTableViewCell: UITableViewCell {
     // MARK: - Setup
     
     private func setupHierarchy() {
-        
+        addSubview(settingImageView)
     }
     
     private func setupLayout() {
-        
+        settingImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.equalTo(32)
+            make.height.equalTo(35)
+        }
     }
     
     // MARK: - Action
     
     // MARK: - Private methods
+    
+    private func setupBackgroundColor() {
+        guard let title = model?.title else { return }
+        
+        switch title {
+        case "Airplane Mode":
+            settingImageView.backgroundColor = .systemOrange
+        case "Wi-Fi", "Bluetooth", "VPN", "Display & Brightness", "Home Screen", "Accessibility", "Privacy & Security":
+            settingImageView.backgroundColor = .systemBlue
+        case "Mobile Data", "Personal Hotspot", "Battery":
+            settingImageView.backgroundColor = .systemGreen
+        case "Notifications", "Sounds & Haptics", "Face ID & Passcode", "Emergency SOS":
+            settingImageView.backgroundColor = .systemRed
+        case "Focus", "Screen Time":
+            settingImageView.backgroundColor = .blue
+        case "General", "Control Centre":
+            settingImageView.backgroundColor = .lightGray
+        case "Wallpaper":
+            settingImageView.backgroundColor = .systemCyan
+        case "Siri & Search":
+            settingImageView.backgroundColor = .black
+        default:
+            settingImageView.backgroundColor = .black
+        }
+    }
     
     // MARK: - Reuse
 }
